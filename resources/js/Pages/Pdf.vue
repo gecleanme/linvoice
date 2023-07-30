c63a018ff1dd9b4abad465cc6bf2fa3e6df6d9c3
gecleanme <130738860+gecleanme@users.noreply.github.com>
2023-07-30 21:31:04 +0300

    added preview notice on mobile

--- resources/js/Pages/Pdf.vue
+++ resources/js/Pages/Pdf.vue
@@ -42,9 +42,14 @@ const showSuccess = ref(false);
             >Download</button>
 
         </a>
+        </div>
 
 
-    </div>
+        <div class="mt-4 flex md:hidden alert alert-warning bg-orange-400 mx-auto p-3 m-4 border-none w-1/2 space-x-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="stroke-current shrink-0 w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            <span>Switch to Desktop site for an accurate preview</span>
+        </div>
+
     <div class="invoice-box">
         <table>
             <tr class="top">
